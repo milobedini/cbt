2bf5c36c971dbd643df6e4e74ac787bad6734d98
Milo Bedini <milobedini64@gmail.com>
2025-07-10 18:53:03 +0100

    types update

--- src/shared-types/module.ts
+++ src/shared-types/module.ts
@@ -19,3 +19,24 @@ export interface CreateModuleInput {
   title: string
   description?: string
 }
+
+export type AuthResponse = {
+  success: boolean
+  message: string
+  user: {
+    _id: string
+    username: string
+    email: string
+  }
+}
+
+export type RegisterInput = {
+  username: string
+  email: string
+  password: string
+}
+
+export type LoginInput = {
+  identifier: string
+  password: string
+}
