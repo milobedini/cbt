e230298a66e77fce5488f313bcf3fa988c6d4179
Milo Bedini <milobedini64@gmail.com>
2025-08-10 16:54:59 +0100

    name not required

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -58,7 +58,6 @@ const userSchema = new mongoose.Schema<IUser>(
     },
     name: {
       type: String,
-      required: [true, 'Name is required!'],
       trim: true,
       minlength: [2, 'Name must be at least 2 characters long!'],
       maxlength: [50, 'Name cannot exceed 50 characters!'],
