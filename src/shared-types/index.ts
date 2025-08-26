514b0dff0b4e7279f7002703d0ffdbd7614d3325
Milo Bedini <milobedini64@gmail.com>
2025-08-26 09:17:21 +0100

    fixed constants

--- src/shared-types/index.ts
+++ src/shared-types/index.ts
@@ -1,7 +1,2 @@
 export type * from './types' // type-only re-export → no JS emitted
-export {
-  SLOT_START_HOUR,
-  SLOT_END_HOUR,
-  SLOT_STEP_HOURS,
-  LONDON_TZ,
-} from './constants'
+export * from './constants.js'
